1cf659959a0ffab1502acc58f479dcad38ec62ae
RArnold <rarnold@datagraphic.co.uk>
2019-05-22 13:53:24 +0100

    Remove p5.js from head.html and place in individual project htmls

--- resources/fragments/head.js
+++ resources/fragments/head.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     $('head').append($('<div class="kill">').load('/resources/fragments/head.html', function() {
         $('head').append($(this).find('#bootstrap').html());
-        $('head').append($(this).find('#p5js').html());
         $('body').prepend($(this).find('#navbar'));
     }));
     $('.kill').remove();
